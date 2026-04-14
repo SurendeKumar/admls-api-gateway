d6988304653ad76f57a8549131ab0752b400ddbc
Surender Kumar <45120705+SurendeKumar@users.noreply.github.com>
2026-04-14 22:43:43 +0100

    Added adlms microservice - gateway pkg

---
+++ src/main/java/com/adlms/apigateway/AdmlsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.adlms.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmlsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmlsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adlms/apigateway/AdmlsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adlms.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmlsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
